9df8afe646b7b83be7aa48b62ab444f2e599571c
paichiwo <lzerucha@gmail.com>
2024-06-04 20:03:11 +0100

    Create report_hook for testing

--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -1,30 +1,31 @@
-import time
+from datetime import datetime, timedelta
 from pycda.main import PyCDA
 
 if __name__ == '__main__':
 
-    start_time = time.time()
+    download_start_time = datetime.now()
+    last_update_time = datetime.now()
 
     def report_hook(count, block_size, total_size):
-        pass
-        # # progress percentage
-        # progress = min(1.0, float(count * block_size) / total_size)
-        # print(f'progress: {progress:.2f}')
-        #
-        # # download speed
-        # elapsed_time = time.time() - start_time
-        # if elapsed_time > 0:
-        #     speed = (count * block_size) / (1024 * elapsed_time)  # speed in KB/s
-        #     print(f'Download speed: {speed:.2f} KB/s')
+        global last_update_time
 
-        # new
-        # downloaded = count * block_size
-        # percent = min(100, downloaded * 100 / total_size)
-        # print(f"\rDownloading: {percent:.2f}% complete", end='')
+        # download speed
+        elapsed_time = (datetime.now() - download_start_time).total_seconds()
+        download_speed = (count * block_size) / (1024 * elapsed_time)
+
+        # progress
+        progress = min(1.0, count * block_size / total_size)
+
+        current_time = datetime.now()
+        if (current_time - last_update_time) >= timedelta(seconds=1):
+            print(f'\rDownloading: {progress:.2f} complete', end='')
+            print(f' | Speed: {download_speed:.2f} KB/s', end='')
+            last_update_time = current_time
 
     url1 = 'https://www.cda.pl/video/14967539bc'
     url2 = 'https://www.cda.pl/video/192935076e'
-    url3 = 'https://www.cda.pl/video/295066274/'
+    url3 = 'https://www.cda.pl/video/295066274'
+    url4 = 'https://www.cda.pl/video/210880265'
 
-    cda = PyCDA(url1)
-    cda.download(on_progress_callback=report_hook)
+    cda = PyCDA(url4)
+    # cda.download(on_progress_callback=report_hook)
