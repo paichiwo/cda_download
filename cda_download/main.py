7280320bcf8f25ee00414bb8fad3d0984b9ad8ab
paichiwo <lzerucha@gmail.com>
2025-02-16 18:58:28 +0000

    Fix chromedriver issues, use new driver

--- cda_download/main.py
+++ cda_download/main.py
@@ -15,7 +15,7 @@ class CdaDownload:
 
         self.__ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0'
         self.__options = webdriver.ChromeOptions()
-        self.__options.add_argument('headless=old')
+        self.__options.add_argument('headless')
         self.__options.add_argument(f'user-agent={self.__ua}')
 
         self.__driver = webdriver.Chrome(options=self.__options)
